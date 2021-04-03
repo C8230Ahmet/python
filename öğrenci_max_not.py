91e618adb5a36e40073d3a26af68e5d547f24a99
C8230Ahmet <80002544+C8230Ahmet@users.noreply.github.com>
2021-04-03 18:33:10 +0300

    Add files via upload

---
+++ öğrenci_max_not.py
@@ -0,0 +1,5 @@
+scores = [99, 87, 78, 86, 68, 94, 76, 97, 56, 98, 76, 87, 79, 90, 73, 93, 82, 69, 97, 98]
+max_not = max(scores)
+kişi_sayısı = scores.count(max_not)
+print("En yüksek not :" + str(max_not))
+print("Notu alan kişi sayısı :" + str(kişi_sayısı))
