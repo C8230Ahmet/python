7b2d79e230515718cb0a4f57289ef066db3ba37d
C8230Ahmet <80002544+C8230Ahmet@users.noreply.github.com>
2021-04-09 20:57:43 +0300

    fibonacci

---
+++ fibonacci.py
@@ -0,0 +1,11 @@
+nterms = int(input("Kaçıncı sayıya kadar? "))
+n1, n2 = 0, 1
+count = 0
+print("Fibonacci :")
+while count < nterms:
+       print(n1)
+       nth = n1 + n2
+       # update values
+       n1 = n2
+       n2 = nth
+       count += 1
