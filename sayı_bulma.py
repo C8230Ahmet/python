3f601f8246172148de72a242c62061b4b414689e
C8230Ahmet <80002544+C8230Ahmet@users.noreply.github.com>
2021-04-23 22:04:42 +0300

    password.py

--- sayı_bulma.py
+++ sayı_bulma.py
@@ -1,12 +1,12 @@
 import random
-def sayı(x):
-    sayı = random.randint(1, x)
-    tahmin = 0
-    while tahmin != sayı:
-        tahmin = int(input(f'1 ile {x} arasında bir sayı tahmin et :'))
-        if tahmin < sayı:
-            print("Çok az söyledin")
-        elif tahmin > sayı:
-            print(("Çok söyledin"))
-    print("Bravo sayıyı buldun")
-sayı(10)
+x = 10
+sayı = random.randint(1, x)
+tahmin = int(input("Tuttuğum sayıyı bul: "))
+while tahmin != sayı:
+    tahmin = int(input("Tuttuğum sayıyı bul: "))
+    if tahmin > sayı:
+     print("Çok yukarı gittin")
+    elif tahmin < sayı :
+        print("Çok az söyledin")
+    if tahmin == sayı :
+        print("Doğru bildin")
